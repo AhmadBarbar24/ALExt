5ea4c5783852d24f19184ebae525d483f89bcf44
Alexander Holstrup <aholstrup@microsoft.com>
2024-03-01 08:06:21 +0100

    Write env variables

--- Actions/DumpWorkflowInfo/DumpWorkflowInfo.ps1
+++ Actions/DumpWorkflowInfo/DumpWorkflowInfo.ps1
@@ -1,4 +1,11 @@
 ﻿Write-Host "Event name: $env:GITHUB_EVENT_NAME"
+
+# Print all env variables 
+Write-Host "Environment variables:"
+$eventPath = Get-Content -Encoding UTF8 -Path $env:GITHUB_EVENT_PATH -Raw
+Write-Host $eventPath
+$eventPath | ConvertFrom-Json | Format-List | Out-String
+
 if ($env:GITHUB_EVENT_NAME -eq 'workflow_dispatch') {
   Write-Host "Inputs:"
   $eventPath = Get-Content -Encoding UTF8 -Path $env:GITHUB_EVENT_PATH -Raw | ConvertFrom-Json
